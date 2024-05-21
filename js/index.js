743927aee1104a3bb449cc2aafe66a5b80098aef
qodirov-a-0110 <qodirovabdumajid03@gmail.com>
2024-05-21 23:07:57 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,61 @@
+// 1-misol
+// let A = +prompt("musbatlikka tekshirish");
+// if (A > 0) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// 2-misol
+// let A = +prompt("toqliga tekshirin tekshirish");
+// if (A % 2 == 1) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// 3-misol
+// let A = +prompt("juftlikka tekshirin tekshirish");
+// if (A % 2 == 0) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// 4-misol
+// let A = +prompt("rostlikka tekshirish");
+// let B = +prompt("rostlikka tekshirish");
+// if (A>2 && B<=3) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// 5-misol
+// let A = +prompt("rostlikka tekshirish");
+// let B = +prompt("rostlikka tekshirish");
+// if (A>=0 || B<-2) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// 6-misol
+// let A = +prompt("rostlikka tekshirish");
+// let B = +prompt("rostlikka tekshirish");
+// let C = +prompt("rostlikka tekshirish");
+// if (A<=B && B<=C && A<=C) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
+
+// 7-misol
+// let A = +prompt("rostlikka tekshirish");
+// let B = +prompt("rostlikka tekshirish");
+// let C = +prompt("rostlikka tekshirish");
+// if (A<=B && B<=C && A<=C) {
+//     console.log(true);
+// }else {
+//     console.log(false);
+// }
